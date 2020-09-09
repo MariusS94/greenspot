4749015756d3709c10a0c2e8d3dca9a4d249f7bd
MariusS94 <marius1spomer@gmail.com>
2020-09-09 10:49:44 +0200

    Accept changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
@@ -10,6 +13,14 @@ app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
+
+app.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+app.use(router);
+app.use(middlewares);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
