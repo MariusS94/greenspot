2446cc2e180d4cbb4735c53301f668c639e148a9
MariusS94 <marius1spomer@gmail.com>
2020-09-15 15:26:39 +0200

    Add routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/welcome"></Route>
+          <Route path="/home"></Route>
+          <Route path="/menue"></Route>
+          <Route path="/forecast"></Route>
+          <Route path="/forecast/zipcode"></Route>
+          <Route path="/">
+            <Redirect to="/welcome"></Redirect>
+          </Route>
+          <Route></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
