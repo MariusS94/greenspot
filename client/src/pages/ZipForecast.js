3b70f66b02123c43a580dbbb43752a60bcc3a199
MariusS94 <marius1spomer@gmail.com>
2020-09-17 14:36:24 +0200

    Add fetching forecast to zipForecast page

--- client/src/pages/ZipForecast.js
+++ client/src/pages/ZipForecast.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchForecast } from "../api/fetchForecast";
+import { useParams } from "react-router-dom";
+import LoadingScreen from "../components/LoadingScreen";
+import ErrorScreen from "../components/ErrorScreen";
 
 const ZipForecast = () => {
-  return <div>forecast charts</div>;
+  const { zip } = useParams();
+  const [forecastData, setForecastData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+        const fetchedForecast = await fetchForecast(zip);
+        setForecastData(fetchedForecast);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [zip]);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  if (error) {
+    return <ErrorScreen />;
+  }
+
+  console.log(forecastData);
+
+  return <div>Forecast</div>;
 };
 
 export default ZipForecast;
