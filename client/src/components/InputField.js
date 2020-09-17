76cade2f61f09ef78601f7db78029e9579740c1c
MariusS94 <marius1spomer@gmail.com>
2020-09-17 22:25:09 +0200

    Add propTypes to InputField

--- client/src/components/InputField.js
+++ client/src/components/InputField.js
@@ -1,15 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const InputField = (probs) => {
+const InputField = (props) => {
   return (
     <input
-      type={probs.type}
-      maxLength={probs.maxLength}
-      value={probs.value}
-      onChange={(event) => probs.onChange(event.target.value)}
-      placeholder={probs.placeholder}
+      type={props.type}
+      value={props.value}
+      onChange={(event) => props.onChange(event.target.value)}
+      placeholder={props.placeholder}
     />
   );
 };
 
 export default InputField;
+
+InputField.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+};
