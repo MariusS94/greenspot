377a7558598e1ac6e3c5a8bce046a5c17c038585
MariusS94 <marius1spomer@gmail.com>
2020-09-22 23:05:01 +0200

    Build BulpLogo componet and and change bulp.svg name

---
+++ client/src/components/BulpLogo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bulp from "../assets/bulp.svg";
+import styled from "@emotion/styled";
+
+const BulpIcon = styled.img`
+  height: 4rem;
+  width: auto;
+  padding: 1.7rem 0;
+`;
+
+const BulpLogo = () => {
+  return (
+    <>
+      <Link to="/home">
+        <BulpIcon src={bulp} alt="Bulp logo" />
+      </Link>
+    </>
+  );
+};
+
+export default BulpLogo;
