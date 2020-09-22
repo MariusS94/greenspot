6b9ba2f3a7b6c77906e7916088ace8180173aafd
MariusS94 <marius1spomer@gmail.com>
2020-09-22 23:09:17 +0200

    Add BulpLogo component to Forecast page and clean code

--- client/src/pages/Forecast.js
+++ client/src/pages/Forecast.js
@@ -2,19 +2,30 @@ import React from "react";
 import styled from "@emotion/styled";
 import windturbine from "../assets/windpower.png";
 import Footer from "../components/Footer";
+import BulpLogo from "../components/BulpLogo";
 
-const BackgroundContainer = styled.div`
+const BackgroundContainer = styled.main`
   background-image: url(${windturbine});
   background-size: cover;
   height: 100vh;
-  filter: blur(5px) saturate(1.2);
+`;
+
+const ContentContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  backdrop-filter: blur(5px) saturate(1.2);
 `;
 
 const Forecast = () => {
   return (
     <>
-      <BackgroundContainer />
-      <Footer />
+      <BackgroundContainer>
+        <ContentContainer>
+          <BulpLogo />
+          <Footer />
+        </ContentContainer>
+      </BackgroundContainer>
     </>
   );
 };
