962c8f7b36d2315c461905e3154af821135af4ea
MariusS94 <marius1spomer@gmail.com>
2020-09-22 22:02:42 +0200

    Add Footer and Background to Forecast page.

--- client/src/pages/Forecast.js
+++ client/src/pages/Forecast.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "@emotion/styled";
+import windturbine from "../assets/windpower.png";
+import Footer from "../components/Footer";
+
+const BackgroundContainer = styled.div`
+  background-image: url(${windturbine});
+  background-size: cover;
+  height: 100vh;
+  filter: blur(5px) saturate(1.2);
+`;
 
 const Forecast = () => {
-  return <div>Forecast</div>;
+  return (
+    <>
+      <BackgroundContainer />
+      <Footer />
+    </>
+  );
 };
 
 export default Forecast;
