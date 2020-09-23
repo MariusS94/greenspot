1b7a4721bfa775363817643ff1df8c5a7cf2901d
MariusS94 <marius1spomer@gmail.com>
2020-09-23 18:15:21 +0200

    Add current gsi to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchCurrentGsi } from "../api/fetchForecast";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [currentGsi, setCurrentGsi] = useState(null);
+
+  useEffect(() => {
+    const zip = localStorage.getItem("zip") || "53842";
+    async function doFetch() {
+      const currentGsi = await fetchCurrentGsi(zip);
+      setCurrentGsi(currentGsi);
+    }
+    doFetch();
+  }, []);
+
+  return (
+    <div>
+      Home
+      <div>{currentGsi}</div>
+    </div>
+  );
 };
 
 export default Home;
