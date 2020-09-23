a5d69067852615a26da9040d44f8f9a46ed1f943
MariusS94 <marius1spomer@gmail.com>
2020-09-24 00:00:44 +0200

    Write fetch function to get city for homepage

--- client/src/api/fetchForecast.js
+++ client/src/api/fetchForecast.js
@@ -25,3 +25,17 @@ export async function fetchCurrentGsi(zip) {
   const zipForecast = await fetchForecast(zip);
   return zipForecast.energyPropForecast[0].gsi;
 }
+
+export async function fetchCity(zip) {
+  const zipCity = await fetchForecast(zip);
+  return zipCity.city;
+}
+
+/* export async function fetchCurrentGsi(zip) {
+  const currentInfo = await fetchForecast(zip);
+  
+  
+  const CityGsi = {gsi: currentInfo.energyPropForecast[0].gsi,
+    city: 
+}
+ */
