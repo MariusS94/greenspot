2b2113931ba0a5c983a987993a23dc3c7c2f69bc
MariusS94 <marius1spomer@gmail.com>
2020-09-23 21:02:53 +0200

    Outsource ContentContainer component

--- client/src/pages/Forecast.js
+++ client/src/pages/Forecast.js
@@ -1,15 +1,8 @@
 import React from "react";
-import styled from "@emotion/styled";
 import Footer from "../components/Footer";
 import BulpLogo from "../components/BulpLogo";
 import BackgroundContainer from "../components/BackgroundContainer";
-
-const ContentContainer = styled.div`
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  backdrop-filter: blur(5px) saturate(1.2);
-`;
+import ContentContainer from "../components/ContentContainer";
 
 const Forecast = () => {
   return (
