b6bf34a355f6842bdb77edfad60e2f7f0fa9f7a4
MariusS94 <marius1spomer@gmail.com>
2020-09-24 21:41:13 +0200

    Adjust ContentContainer style for EnergyInfo

--- client/src/components/ContentContainer.js
+++ client/src/components/ContentContainer.js
@@ -3,7 +3,9 @@ import styled from "@emotion/styled";
 const ContentContainer = styled.div`
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   backdrop-filter: blur(5px) saturate(1.2);
 `;
 
