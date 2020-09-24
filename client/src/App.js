b7b030a1568d228b8b204160e750bd58b5a929d5
MariusS94 <marius1spomer@gmail.com>
2020-09-24 16:11:55 +0200

    Adjust paths and add note page

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,8 @@ import {
 } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
-import Menue from "./pages/Menue";
 import Forecast from "./pages/Forecast";
-import ZipForecast from "./pages/ZipForecast";
-import ZipInput from "./pages/ZipInput";
+import Note from "./pages/Note";
 
 function App() {
   return (
@@ -21,24 +19,23 @@ function App() {
           <Route path="/welcome">
             <Welcome />
           </Route>
-
-          <Route path="/home">
+          <Route path="/energy/home">
             <Home />
           </Route>
-          <Route path="/menue">
-            <Menue />
-          </Route>
-          <Route exact path="/forecast">
+          <Route path="/energy/forecast/:zip">
             <Forecast />
           </Route>
-          <Route path="/forecast/:zip">
-            <ZipForecast />
+          <Route path="/news/home">
+            <Note />
           </Route>
-          <Route path="/zipinput">
-            <ZipInput />
+          <Route path="/mobility/home">
+            <Note />
+          </Route>
+          <Route path="/options">
+            <Note />
           </Route>
           <Route path="/">
-            <Redirect to="/welcome"></Redirect>
+            <Redirect to="/energy/home"></Redirect>
           </Route>
           <Route></Route>
         </Switch>
---
+++ client/src/pages/Note.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Note = () => {
+  return <div>Noch in Bearbeitung</div>;
+};
+
+export default Note;
