361c8773293a5a723de9c97d6ef31d21d780c4e8
MariusS94 <marius1spomer@gmail.com>
2020-09-30 14:49:19 +0200

    Add Zip input pop up to Forecast Chart

--- client/src/components/ForecastChart.js
+++ client/src/components/ForecastChart.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContentContainer from "./ContentContainer";
+import ZipInputPopUp from "./ZipInputPopUp";
+import { useParams } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const ZipButton = styled.button`
+  border: none;
+  padding: 0.2rem;
+  margin-top: 1rem;
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid;
+  color: #ffffff;
+`;
 
 export const ForecastChart = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const zip = useParams();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [zip]);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <ContentContainer>
-      <div></div>
+      {!isOpen && <ZipButton onClick={togglePopup}>PLZ Eingabe</ZipButton>}
+      {isOpen && <ZipInputPopUp handleClose={togglePopup} />}
     </ContentContainer>
   );
 };
